8b04a3a3259d36e89aefcf0605a3f7ffd4204164
R.SIVAPACKIA <sivapackia2001@gmail.com>
2024-03-16 18:03:18 +0530

    setes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/company-task/",
+  base:"/company-task",
   plugins: [react()],
 })
